df6685d282ded31c129877e029dec8f62e2ded93
Alex Rokachev <alserokachev@gmail.com>
2023-01-25 16:34:24 +0300

    made ex3

---
+++ folder/ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int RandomMass()
+{
+    int zero = 0;
+    Random rand = new Random();
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i++)
+    {
+        array[i] = rand.Next(1, 101);
+    }
+    System.Console.WriteLine("Массив: " + string.Join(", ", array));
+    return zero;
+}
+
+RandomMass();
